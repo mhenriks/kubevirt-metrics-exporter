82d83f551b38038c650af4c5a7f3c9af18a10ec3
Michal Skrivanek <michal.skrivanek@redhat.com>
2026-09-08 14:16:39 +0200

    fix linting
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- pkg/cgroup/buddyinfo.go
+++ pkg/cgroup/buddyinfo.go
@@ -13,9 +13,9 @@ const buddyPageSize = 4096
 
 // numaBuddyFree holds exact free buddy block counts for the Normal zone per NUMA node.
 type numaBuddyFree struct {
-	NUMA              string
-	OrderGe9Bytes     uint64
-	AllOrdersBytes    uint64
+	NUMA           string
+	OrderGe9Bytes  uint64
+	AllOrdersBytes uint64
 }
 
 // readBuddyNormal parses /proc/buddyinfo free block counts for the Normal zone per NUMA node.
